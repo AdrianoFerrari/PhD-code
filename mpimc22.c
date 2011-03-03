1491c8035412881004afb4a0cdb43750cf4efd7d
aferrari <aferrari@gpc-f102n084.(none)>
2011-03-03 11:48:01 -0500

    GrowthRate function seems to be working.

--- mpimc22.c
+++ mpimc22.c
@@ -273,7 +273,7 @@ accValsTested++;
    		 }
 		 fprintf(ffp,"%f,%f,%f,%f\n",FplL,FplR,FrljL,FrljR);
 
-	         double g1 = GrowthRate(0.5,2.0,k[0][0],k[1][0],k[2][0],-0.5*pars.R,0.0,pars);
+	         double g1 = GrowthRate(0.04166666,2.0,k[0][0],k[1][0],k[2][0],-0.5*pars.R,0.0,pars);
                  fprintf(grwth,"%f, %f\n",0.5,g1);
 		}
 	}
