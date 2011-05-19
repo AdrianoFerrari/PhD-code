08904a3b8afb4d6c6219c42b7c562c88aef41f90
aferrari <aferrari@gpc-f104n084.(none)>
2011-05-19 19:30:19 -0400

    added RNG seeding capabilities

--- main.h
+++ main.h
@@ -7,17 +7,18 @@
 /* KISS RNG 
 https://groups.google.com/group/comp.lang.fortran/browse_thread/thread/a85bf5f2a97f5a55?fwc=2&hl=en
 */
-static unsigned long long 
-x=1234567890987654321ULL,c=123456123456123456ULL, 
-  y=362436362436362436ULL,z=1066149217761810ULL,t; 
+
 #define MWC (t=(x<<58)+c, c=(x>>6), x+=t, c+=(x<t), x) 
 #define XSH ( y^=(y<<13), y^=(y>>17), y^=(y<<43) ) 
 #define CNG ( z=6906969069LL*z+1234567 ) 
 #define KISS (MWC+XSH+CNG) 
+typedef unsigned long long ULL;
+static ULL x=1234567890987654321ULL,c=123456123456123456ULL, y=362436362436362436ULL,z=1066149217761810ULL,t; 
+
+void settable(ULL i1, ULL i2, ULL i3, ULL i4)
+{ 
+  x = i1; c = i2; y = i3; z = i4;
+}
 
 const double uy = 0.041666666666;
 const int M = 7;//Lekner terms
-
-//double dummy_function();
-//double lekner_potential(double q0, double x0, double y0, double z0, double q1, double x1, double y1, double z1 );
-//double repulsive_potential(double x0,double y0,double z0,double x1,double y1,double z1, double epsilon);
--- main_test.c
+++ main_test.c
@@ -10,18 +10,12 @@ static void test_dummy_function() {
 static void test_lekner_potential_1() {
   double val = lekner_potential( 1.0, 1.0, 1.0, 1.0, -1.0, -1.0, -1.0, -1.0 );
   double expected = -0.0142195;
-  assert(val >= expected*1.005
-	 &&
-	 val <= expected*0.995
-	 );
+  assert(val >= expected*1.005 && val <= expected*0.995);
 }
 static void test_lekner_potential_2() {
   double val = lekner_potential( 1.0, 2.0, 3.0, 4.0, 4.0, 3.0, 2.0, 1.0 );
   double expected = 0.105726;
-  assert(val >= expected*0.995
-	 &&
-	 val <= expected*1.005
-	 );
+  assert(val >= expected*0.995 && val <= expected*1.005);
 }
 static void test_repulsive_potential_1() {
   assert(repulsive_potential(1.23, 4.21, -5.0, 3.22, 1.044, 0.0, 23) == 0.0);
@@ -29,24 +23,25 @@ static void test_repulsive_potential_1() {
 static void test_repulsive_potential_2() {
   double val = repulsive_potential(1.23,4.21,-1.0,3.22,1.044,0.0,23);
   double expected = 0.00000203246;
-  assert(val >= expected*0.995
-	 &&
-	 val <= expected*1.005
-	 );
+  assert(val >= expected*0.995 && val <= expected*1.005);
 }
 static void test_repulsive_potential_3() {
   double val = repulsive_potential(0.35,0.90,0.70,-0.27,-0.07,0.49,0.81);
   double expected = 0.12283;
-  assert(val >= expected*0.995
-	 &&
-	 val <= expected*1.005
-	 );
+  assert(val >= expected*0.995 && val <= expected*1.005);
 }
 static void test_repulsive_potential_4() {
   assert(repulsive_potential(1.0,1.0,1.0,1.0,1.0,1.0,1.0) == 10e20);
 }
 static void test_kiss_rng() {
   int i; 
+  for(i=0;i<100000000;i++) t=KISS;
+  assert(t==1666297717051644203ULL);
+}
+static void test_kiss_rng_seed() {
+  int i; 
+  for(i=0;i<1000;i++) t=KISS;  
+  settable(1234567890987654321ULL, 123456123456123456ULL, 362436362436362436ULL, 1066149217761810ULL);
   for(i=0;i<100000000;i++) t=KISS;
   assert(t==1666297717051644203ULL);
 }
@@ -100,8 +95,9 @@ int main() {
   //test_repulsive_potential_3();
   //test_repulsive_potential_4();
   //test_kiss_rng();
-  test_pair_potential_energy_1();  
-  test_pair_potential_energy_2();
-  test_single_particle_energy_1();  
-  test_single_particle_energy_2();
+  test_kiss_rng_seed();
+  //test_pair_potential_energy_1();  
+  //test_pair_potential_energy_2();
+  //test_single_particle_energy_1();  
+  //test_single_particle_energy_2();
 }
