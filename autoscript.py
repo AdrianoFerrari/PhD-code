e212c3e724912a3b88e9fb0294e01a42df9c06d3
aferrari <aferrari@gpc-f103n084.(none)>
2011-10-06 15:11:58 -0400

    working autoscripts

--- autoscript.py
+++ autoscript.py
@@ -18,8 +18,8 @@ def generate_jobs(Ns,charges,eps,Ts,t0s,tFs,Rs,As,wlengths,Gos,Fos,Pos):
                                             for Po in Pos:
                                                 for R in Rs:
                                                     f = open('mpijob' + str(i) + '.pbs', 'w')
-                                                    filename = "N%d%se%sti%stF%sR%sA%sw%s" % (N,de(charge),de(ep),de(t0),de(tF),de(R),de(A),de(wlength))
-                                                    s = "#!/bin/bash\n#PBS -N %s\n#PBS -l nodes=4:ppn=8,walltime=0:20:00\n\ncd $PBS_O_WORKDIR\n" % filename
+                                                    filename = "N%dtF%sR%sA%sw%s" % (N,de(tF),de(R),de(A),de(wlength))
+                                                    s = "#!/bin/bash\n#PBS -N %s\n#PBS -l nodes=4:ppn=8,walltime=0:00:40\n\ncd $PBS_O_WORKDIR\n" % filename
                                                     s = s + "mpirun -np 32 ./main.exe " + str(N) + " " + str(charge) + " " + str(ep) + " " + str(T) + " " + str(t0) + " " + str(tF) + " " + str(R) + " " + str(A) + " " + str(wlength) + " " + filename + " " + str(Go)+ " " + str(Fo)+ " " + str(Po)
                                                     f.write(s)
                                                     f.close
@@ -27,4 +27,4 @@ def generate_jobs(Ns,charges,eps,Ts,t0s,tFs,Rs,As,wlengths,Gos,Fos,Pos):
                                                     fscript.writelines("\t".join([str(R),str(A),filename])+"\n")
     fscript.close
 
-generate_jobs([64],[2.3],[3.356],[100000],[5.0],[0.001],[0.1,0.5,1.0,2.0,7.0],[0.01,0.0],[4.0],[10000],[10000],[0])
+generate_jobs([32],[2.3],[3.356],[400000],[5.0],[0.001],[0.1,0.5,1.0,2.0,7.0],[0.01,0.0],[4.0],[0],[5000],[0])
--- process.py
+++ process.py
@@ -1,9 +1,8 @@
 import sys, glob, numpy
 
 def concatenate(base):
-    tf = open(base+'.growth.all','w')
-    for f in glob.glob(base+".*.growth"):
-        print f
+    tf = open(base+'.force.all','w')
+    for f in glob.glob(base+".*.force"):
         tf.write(open(f).read())
     tf.close()
         
@@ -19,7 +18,8 @@ def get_clean_stats(filename):
     while points_removed:
         avg = numpy.average(list)
         stdev = numpy.std(list)
-    
+        print avg, stdev
+
         points_removed = False
     
         for p in list:
@@ -28,3 +28,21 @@ def get_clean_stats(filename):
                 list.remove(p)
                 points_removed = True
     return [avg,stdev]
+
+#Main script
+fjd = open('job_data')
+fjdout = open('job_data.csv','w')
+
+for line in fjd:
+    if line.startswith("R\t"):
+        fjdout.write(line)
+    else:
+        base = line.split("\t")[-1][0:-1]
+        print base
+        concatenate(base)
+        stats = get_clean_stats(base+'.force.all')
+        stats_string = [str(stats[0]),str(stats[1])]
+        
+        fjdout.write(line[0:-1]+"\t"+"\t".join(stats_string)+"\n")
+
+fjdout.close()
